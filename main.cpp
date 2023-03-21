6ec5dfb69c1469795a46c83c9d746cdb03da0dc1
link16992 <link16992@gmail.com>
2023-03-21 08:40:13 +0700

    Add username

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    // Nguyen Hoang Trung
+
     int b[MAX_ARRAY] = {0, 2, 1, 4, 3, 6, 5, 8, 7, 9};
     sort(b, b+MAX_ARRAY, compare);
 
